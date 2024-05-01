e2207920b2efe8cebcc065c0e9128af06cc2be76
qazilaiba08 <164245574+qazilaiba08@users.noreply.github.com>
2024-05-01 15:49:16 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+// console.log('hello world')
+// console.log('laiba qazi')
+// document.write("hello world")
+
+// alert('Welcome To Our Website')
+console.log('careful writer ends every declaretive sentence with a period');
+
+// var first_name = "Laiba Qazi";
+
+
+// let last_name = 'qazi';
+// console.log(first_name);
+// console.log(last_name);
+//  var first_name; //declair
+//  first_name = 'Laiba';
+//  var first_name = "Laiba"
+
+var text = "it is invented in 1845";
+console.log(text);
+
+var name = "laiba";
+var email = "laibaqazi@gmail.com"
+var dateOfBirth = 2002;
+console.log(name +"  "+ email +"  "+ dateOfBirth);
+console.log(email);
+console.log(dateOfBirth)
+
